314e3d9ada912b3f0a1c9c71ff82ae47d8b985e2
Bhumika Saini <bs98.saini@gmail.com>
2018-04-23 17:57:17 +0530

    Base code

---
+++ Other Codes/Black_line_follower/Black_line_follower.ino
@@ -0,0 +1,32 @@
+int  l,r;
+void setup() {
+  pinMode(2,INPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  
+  pinMode(3,INPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
+  
+  
+ 
+
+}
+
+void loop()
+{
+ a= digitalRead(4);
+ if(a==HIGH)
+    { digitalWrite(2,HIGH);
+      digitalWrite(3,LOW);
+      delay(1000);
+      
+ }
+ else if(a==LOW)
+ {
+   digitalWrite(3,LOW);
+      digitalWrite(2,LOW);
+      delay(1000);
+    
+}
+}
---
+++ pwm_with_pot/pwm_with_pot.ino
@@ -0,0 +1,33 @@
+/*PWM Pins for Atmega 328
+9
+10
+11
+3
+5
+6
+*/
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+ 
+}
+ 
+void loop()
+{  
+   int a=analogRead(A0);
+   int pwm = map(a, 0, 1024, 0, 255); 
+   Serial.println(a);
+ 
+   analogWrite(9,pwm);
+   analogWrite(10,0); 
+   delay(50);
+  
+}
+
+
+
+
